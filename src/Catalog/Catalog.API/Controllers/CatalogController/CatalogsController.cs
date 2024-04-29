7d66e420de01e5c74e405d2e1350045bf261f06d
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-04-29 17:34:34 +0500

    Add project files.

---
+++ src/Catalog/Catalog.API/Controllers/CatalogController/CatalogsController.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Application.UseCases.CatalogCases.Commands;
+using Catalog.Application.UseCases.CatalogCases.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers.CatalogController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CatalogsController(IMediator mediator)
+        {
+             _mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public async  Task<IActionResult> CreateCatalog(CreateCatalogCommand command)
+        {
+           var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCatalogs()
+        {
+            var result = await _mediator.Send(new GetAllCatalogsQuery());
+            return Ok(result);
+        }
+    }
+}
